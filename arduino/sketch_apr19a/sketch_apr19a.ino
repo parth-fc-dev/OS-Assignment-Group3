6f617ce39aa43f0851e94beaa68a5350122f382f
Parth Chokshi <145736348+parth-fc-dev@users.noreply.github.com>
2024-04-19 19:54:34 +0530

    Create sketch_apr19a.ino

---
+++ arduino/sketch_apr19a/sketch_apr19a.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+int actualValue = 0;
+void setup()
+{
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0)*2.5/100;
+  // print out the value you read:
+  if(sensorValue < actualValue -1 || sensorValue > actualValue + 2  ){
+    actualValue = sensorValue;
+    if(actualValue > 100){
+      actualValue = 100;
+    }
+    Serial.println(actualValue);
+  }
+  // int actualValue = sensorValue * 2.5;
+  delay(100); // delay in between reads for stability
+}
